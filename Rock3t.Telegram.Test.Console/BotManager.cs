95b335472344845b7fbecc1440de4d49789c23ff
Leonie Seifert <mail@rock3t.de>
2022-09-26 08:04:28 +0200

    some improvements

---
+++ Rock3t.Telegram.Test.Console/BotManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Rock3t.Telegram.Test.Console;
+
+public class BotManager
+{
+
+
+    private ILogger<BotManager> _logger;
+
+    public BotManager(ILogger<BotManager> logger)
+    {
+        _logger = logger;
+    }
+}
