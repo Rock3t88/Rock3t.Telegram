c6fe4175fdeb5e872333b6ac61704e19f7692c6e
Leonie Seifert <mail@rock3t.de>
2022-10-06 22:38:55 +0200

    hää

---
+++ Rock3t.Telegram.Lib/UserItemMessageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot.Types;
+
+namespace Rock3t.Telegram.Lib;
+
+public static class UserItemMessageExtensions
+{
+    public static bool ContainsMessageId(this Dictionary<long, List<int>> dict, Message? message)
+    {
+        if (message?.From?.Id == null)
+            return false;
+
+        return dict.ContainsKey(message.From.Id);
+    }
+
+    public static bool ContainsUserId(this Dictionary<long, List<int>> dict, Message? message)
+    {
+        if (message == null)
+            return false;
+
+        foreach (var values in dict.Values)
+        {
+            if (values.Contains(message.MessageId)) 
+                return true;
+        }
+
+        return false;
+    }
+}
